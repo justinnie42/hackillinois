f881a85712a0e8b88d89e6f42d45f919ff175597
brandonfzhang <fireblaze7683@gmail.com>
2022-02-26 00:02:10 -0600

    made a function that renders a question

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
@@ -7,9 +8,20 @@ function App() {
         <h1>
           Dorm Quiz
         </h1>
-        <input type="radio" value="Submit"/>
+        <div>{RenderQuestion("Test question", ["Test answer"])}</div>
     </div>
   );
+}
+
+function RenderQuestion(question, answers){
+  const list = []
+    for (const [i, answer] of answers.entries()) {
+      list.push(<div><input type="radio" value={answer}/>{answer}</div>)
+    }
+
+    return (<div><div>{question}</div>
+            <div>{list}</div></div>
+      );
 }
 
 export default App;
