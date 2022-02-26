58cf2001b999516177b66d9fdbd395f50ece044a
brandonfzhang <fireblaze7683@gmail.com>
2022-02-26 00:52:34 -0600

    page can now record and process input

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,40 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-function App() {
-  <link href = "App.css" rel = "stylesheet"/>
-  return (
-    <div class = "Container">
-        <h1>
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {    this.setState({value: event.target.value});  }
+  handleSubmit(event) {
+    alert('An answer was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (<div>
+      <h1>
           Dorm Quiz
         </h1>
-        <div id = "question">Question</div><br></br>
-        <form>
-        <input type = "radio" name = "interest_value" id = "interest1"/>
+      <div id = "question">Question</div><br></br>
+      <form onSubmit={this.handleSubmit}>
+        <input type = "radio" name = "interest_value" id = "interest1" value="Highly Interested" onChange={this.handleChange}/>
         <label for="interest1">Highly Interested</label><br></br>
-        <input type = "radio" name = "interest_value" id = "interest2"/>
+        <input type = "radio" name = "interest_value" id = "interest2" value="Somewhat Interested" onChange={this.handleChange}/>
         <label for="interest2">Somewhat Interested</label><br></br>
-        <input type = "radio" name = "interest_value" id = "interest3"/>
+        <input type = "radio" name = "interest_value" id = "interest3" value="Neutral" onChange={this.handleChange}/>
         <label for="interest3">Neutral</label><br></br>
-        <input type = "radio" name = "interest_value"  id = "interest4"/>
+        <input type = "radio" name = "interest_value"  id = "interest4" value="Not Very Interested" onChange={this.handleChange}/>
         <label for="interest4">Not Very Interested</label><br></br>
-        <input type = "radio" name = "interest_value"  id = "interest5"/>
+        <input type = "radio" name = "interest_value"  id = "interest5" value="No Interested" onChange={this.handleChange}/>
         <label for="interest5">No Interested</label><br></br>
-        <input type="button" value="Submit"/>
-        </form>
-    </div>
-  );
-}
-
-function RenderQuestion(question, answers){
-  const list = []
-    for (const [i, answer] of answers.entries()) {
-      list.push(<div><input type="radio" value={answer}/>{answer}</div>)
-    }
-
-    return (<div><div>{question}</div>
-            <div>{list}</div></div>
-      );
+        <input type="submit" value="Submit"/>
+        </form></div>
+    );
+  }
 }
 
 export default App;
