968d813c783c28031b8d5a95926d5053ebc9a1f7
justinnie42 <justin.nie42@gmail.com>
2022-02-26 00:10:53 -0600

    Fixed interest button
    Only shows 1 interest

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,21 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  <link href = "App.css" rel = "stylesheet"/>
   return (
-    <div className="App">
+    <div class = "Container">
         <h1>
           Dorm Quiz
         </h1>
+        <div id = "question">Question</div><br></br>
         <form>
-        <input type = "radio" id = "interest1"/>
+        <input type = "radio" name = "interest_value" id = "interest1"/>
         <label for="interest1">Highly Interested</label><br></br>
-        <input type = "radio" id = "interest2"/>
+        <input type = "radio" name = "interest_value" id = "interest2"/>
         <label for="interest2">Somewhat Interested</label><br></br>
-        <input type = "radio" id = "interest3"/>
+        <input type = "radio" name = "interest_value" id = "interest3"/>
         <label for="interest3">Neutral</label><br></br>
-        <input type = "radio" id = "interest4"/>
+        <input type = "radio" name = "interest_value"  id = "interest4"/>
         <label for="interest4">Not Very Interested</label><br></br>
-        <input type = "radio" id = "interest5"/>
+        <input type = "radio" name = "interest_value"  id = "interest5"/>
         <label for="interest5">No Interested</label><br></br>
         <input type="button" value="Submit"/>
         </form>
